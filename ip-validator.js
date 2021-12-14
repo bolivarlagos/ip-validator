8271b2122ab352b047f37f7358e742d2350fd22f
bolivarlagos <bolivarlagos@gmail.com>
2021-12-14 20:29:07 -0300

    reading the file

--- ip-validator.js
+++ ip-validator.js
@@ -13,15 +13,60 @@ const arg = process.argv[2]
 const ip4FileName = 'ip4.txt'
 const ip6FileName = 'ip6.txt'
 
-if(process.argv.length > 3){
-    console.log()
+const handleExit = (...emiterArgs) => {
+    switch(emiterArgs[0]){
+        case 'exceed':
+            console.log('Too many arguments')
+            console.log('Try ip-validator --help')
+            break
+        case 'no-file':
+            console.log('No file was provided')
+            console.log('Try ip-validator --help')
+            break          
+        case 'help':
+            console.log('Usage:')
+            console.log('ip-validator <fileName>')
+            console.log('eg: ip-validator ipList.txt')
+            break   
+        case 'error':
+            console.log('Error', emiterArgs[1]) 
+            break 
+        default:
+            console.log('Not expected')
+            break     
+    }
 }
 
+customEmitter.on('exit', handleExit)
+
+if(process.argv.length > 3){
+    customEmitter.emit('exit', 'exceed')
+}
 if(!arg){
-    console.log()
+    customEmitter.emit('exit', 'no-file')
 } else if(arg === '--help'){
-    console.log()
+    customEmitter.emit('exit', 'help')
 } else {
-    console.log()
+    const file = `${__dirname}/${arg}`
+    let allIpv4 = 'List of all the ipv4:\n'
+    let allIpv6 = 'List of all the ipv6:\n'
+
+    fs.readFile(file, (err, data) => {
+        if(err){
+            customEmitter.emit('exit', 'error', err)
+        }
+        const wordCollection = data.toString().split(fileRegex)
+        wordCollection.forEach(item => {
+            if(item.match(ipv4Regex)){
+                allIpv4 += item + '\n'
+                return 
+            }
+            if(item.match(ipv6Regex)){
+                allIpv6 += item + '\n'
+                return 
+            }
+        })
+        console.log(allIpv4, allIpv6)
+    })
 }
 
